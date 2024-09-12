dfbcc3882dee67ce2d467eab2980301cdb9f6e3e
Yushxx <109989261+Yushxx@users.noreply.github.com>
2024-09-12 20:32:25 +0000

    Create sequence.js

---
+++ sequence.js
@@ -0,0 +1,11 @@
+// Fonction pour générer une séquence de jeu Apple
+function generateAppleSequence() {
+    const sequence = ["🟩", "🟩", "🟩", "🟩", "🍎"];
+    for (let i = sequence.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [sequence[i], sequence[j]] = [sequence[j], sequence[i]];
+    }
+    return sequence.join(" ");
+}
+
+module.exports = { generateAppleSequence };
